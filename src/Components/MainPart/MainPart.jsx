da843e83622e0dd1089447ba824c33e4bbad1f6f
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2024-03-07 01:04:02 +0600

    Travel blog part Done

--- src/Components/MainPart/MainPart.jsx
+++ src/Components/MainPart/MainPart.jsx
@@ -1,10 +1,17 @@
 import fet1 from '../../../public/Thumbnail/Rectangle 181.png'
 import fet2 from '../../../public/Thumbnail/Rectangle 18-11.png'
 import fet3 from '../../../public/Thumbnail/Rectangle 18-22.png'
+import fets1 from '../../../public/Thumbnail/Rectangle 18.png'
+import fets2 from '../../../public/Thumbnail/Rectangle 18-1.png'
+import fets3 from '../../../public/Thumbnail/Rectangle 18-2.png'
 import off1 from '../../../public/Offer/sliderUp_to_14__discount_on_Domestic_flights 2-1.png'
 import off2 from '../../../public/Offer/sliderUp_to_14__discount_on_Domestic_flights 2-2.png'
 import off3 from '../../../public/Offer/sliderUp_to_14__discount_on_Domestic_flights 2.png'
 import news from '../../../public/News letter/Isolation_Mode.png'
+import test1 from '../../../public/testimonial/Rectangle 30.png'
+import test2 from '../../../public/testimonial/Rectangle 32.png'
+import test3 from '../../../public/testimonial/Rectangle 33.png'
+import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos, MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 const MainPart = () => {
     return (
@@ -76,6 +83,71 @@ const MainPart = () => {
                     </div>
                 </div>
                 <p className='text-7xl text-center '>.<span className='text-[#ffb700e8]'>.</span>.</p>
+            </div>
+
+            {/* Travel Blog */}
+            <div className='my-10 relative'>
+                <h1 className='text-4xl font-bold'>Travel Blog</h1>
+                <div className='mt-14 grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                 <div className="card card-compact bg-base-100 shadow-2xl">
+                    <figure><img src={fets1} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <div className='flex flex-row gap-5'>
+                            <p className='text-[#FFB700]'>Work and Travel</p>
+                            <p>16 September 23</p>
+                        </div>
+                        <p className='font-bold'>Embracing The Digital Nomad Lifestyle The Digital Nomad</p>
+                        <hr />
+                        <div className='flex flex-row justify-between items-center my-3'>
+                           <div className='flex flex-row items-center gap-2'>
+                           <img className='h-7 w-7 rounded-full' src={test1} alt="" />
+                            <p>Ys Sttafen</p>
+                           </div>
+                            <a className='flex items-center flex-row gap-1' href="">Read More <span className='mt-1 text-lg'><MdOutlineKeyboardDoubleArrowRight /></span></a>
+                        </div>
+                    </div>
+                 </div>
+                 <div className="card card-compact bg-base-100 shadow-2xl">
+                    <figure><img src={fets2} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <div className='flex flex-row gap-5'>
+                            <p className='text-[#FFB700]'>Work and Travel</p>
+                            <p>16 September 23</p>
+                        </div>
+                        <p className='font-bold'>Embracing The Digital Nomad Lifestyle The Digital Nomad</p>
+                        <hr />
+                        <div className='flex flex-row justify-between items-center my-3'>
+                           <div className='flex flex-row items-center gap-2'>
+                           <img className='h-7 w-7 rounded-full' src={test2} alt="" />
+                            <p>Ys Sttafen</p>
+                           </div>
+                           <a className='flex items-center flex-row gap-1' href="">Read More <span className='mt-1 text-lg'><MdOutlineKeyboardDoubleArrowRight /></span></a>
+                        </div>
+                    </div>
+                 </div>
+                 <div className="card card-compact bg-base-100 shadow-2xl">
+                    <figure><img src={fets3} alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <div className='flex flex-row gap-5'>
+                            <p className='text-[#FFB700]'>Work and Travel</p>
+                            <p>16 September 23</p>
+                        </div>
+                        <p className='font-bold'>Embracing The Digital Nomad Lifestyle The Digital Nomad</p>
+                        <hr />
+                        <div className='flex flex-row justify-between items-center my-3'>
+                           <div className='flex flex-row items-center gap-2'>
+                           <img className='h-7 w-7 rounded-full' src={test3} alt="" />
+                            <p>Ys Sttafen</p>
+                           </div>
+                           <a className='flex items-center flex-row gap-1' href="">Read More <span className='mt-1 text-lg'><MdOutlineKeyboardDoubleArrowRight /></span></a>
+                        </div>
+                    </div>
+                 </div>
+                </div>
+                <div className=''>
+                <MdOutlineArrowBackIos className='bg-[#ffb700de] rounded-full w-8 h-8 text-white absolute top-[50%] left-5 cursor-pointer' />
+                <MdOutlineArrowForwardIos className='bg-[#ffb700de] rounded-full w-8 h-8 text-white absolute top-[50%] right-5 cursor-pointer' />
+                </div>
             </div>
 
             {/* Subscribe part */}
